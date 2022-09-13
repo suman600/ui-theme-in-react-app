f4b45bc73a85d8b3af9d95a59b11f76816de1330
Testbook <testbook@sumans-MacBook-Pro.local>
2022-09-14 01:04:56 +0530

    added src file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+//import logo from './assets/imaegs/logo.svg';
+import './app.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return ( <
+        div class = "name" >
+        <
+        h1 > dsfadsf < /h1> <
+        span class = "badge badge-pill" > dfasfas < /span> < /
+        div >
+    );
 }
 
 export default App;
