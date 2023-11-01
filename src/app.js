c5dff68f1fab5f4a2bb11b30770713841d440181
suman600 <sumangupta.engr@gmail.com>
2023-11-01 12:42:34 +0530

    added text

---
+++ src/app.js
@@ -0,0 +1,58 @@
+import '././assets/ui-theme/custom/page-view.scss';
+
+
+import Folder from './folder';
+import Alert from './alert';
+import Badge from './badge';
+import Breadcrumb from './breadcrumb';
+import Button from './button';
+import Card from './card';
+import Carousel from './carousel';
+import Form from './form';
+import Input from './input';
+import Modal from './modal';
+import Navbar from './navbar';
+import Pagination from './pagination';
+import Popovers from './popovers';
+import Progress from './progress';
+import Scrollspy from './scrollspy';
+import Spinners from './spinner';
+import Toasts from './toasts';
+import Dropdowns from './dropdowns';
+import Tooltips from './tooltips';
+import Jumbotron from './jumbotron';
+import Listgroup from './list-group';
+import MediaObject from './media-object';
+import Collapse from './collapse';
+
+function App () {
+    return (
+        <main>
+            <Folder />
+            <Alert />
+            <Badge />
+            <Breadcrumb />
+            <Button />
+            <Card />
+            <Carousel />
+            <Collapse />
+            <Dropdowns />
+            <Form />
+            <Input />
+            <Jumbotron />
+            <Listgroup />
+            <MediaObject />
+            <Modal />
+            <Navbar />
+            <Pagination />
+            <Popovers />
+            <Progress />
+            <Scrollspy />
+            <Spinners />
+            <Toasts />
+            <Tooltips />
+        </main>
+    );
+}
+
+export default App;
